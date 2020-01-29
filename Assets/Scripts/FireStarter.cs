2d6faf58eddd063df37c5df0cdbeebac7e9ca5fe
Dylan Dawkins <dad586@nyu.edu>
2020-01-29 14:44:18 -0500

    add working torch

---
+++ Assets/Scripts/FireStarter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireStarter : MonoBehaviour
+{
+    private int count = 1;
+    [SerializeField]
+    int setTime = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(count);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject objectHit = other.gameObject;
+        Debug.Log(objectHit.name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        count++;
+        if (count == setTime && other.gameObject.CompareTag("burnable"))
+        {
+            SetObjectOnFire(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        count = 0;
+    }
+
+    void SetObjectOnFire(GameObject obj)
+    {
+        Debug.Log("Burnable: " + obj.gameObject.name);
+
+        Component[] fireParticles = obj.GetComponentsInChildren<ParticleSystem>();
+        foreach(ParticleSystem fire in fireParticles)
+        {
+            fire.Play();
+        }
+    }
+
+}
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = .5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.forward * -speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+    }
+}
